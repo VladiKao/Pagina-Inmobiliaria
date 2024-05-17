7d235c82c64d734515dc78a1f2effdb496b87247
VladiKao <kaovladi@gmail.com>
2024-05-17 12:02:21 -0600

    inmobiliaria

---
+++ admin/bd/conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "elnumero1";     // database password
+$dbname	= "entrega24";    // database name
+?>
